765ef97d94b8c82002065da192baffb48ca1f78e
Cyprien023 <cyprienluzuy@gmail.com>
2023-11-29 16:26:31 +0100

    correction bad merge

--- Front/fichtr/src/App.js
+++ Front/fichtr/src/App.js
@@ -1,44 +1,37 @@
-import "./App.css";
 import "./composant/InscriptionForm";
-import InscriptionPage from "./composant/InscriptionPage";
-
-function App() {
-  return <InscriptionPage />;
-import logo from './logo.svg';
-import './App.css';
-import navbarSide from "./Navbar_side.jsx"
-import Toolbar from "./Toolbar.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Toolbar from "./composant/Toolbar.jsx";
 import Root from "./pages/root";
 import Questions from "./pages/questions";
 import Tags from "./pages/tags";
 import Shoutbox from "./pages/shoutbox";
 
 export default function App() {
-  return (
-      <>
+    return (
+        <>
 
         <Toolbar>
-          <div>
-            HexaCorp
-          </div>
+            <div>
+                HexaCorp
+            </div>
         </Toolbar>
 
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+            <div className="App">
+            <header className="App-header">
 
-      </>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Root />}>
-          <Route path="questions" element={<Questions />} />
-          <Route path="tags" element={<Tags />} />
-          <Route path="shoutbox" element={<Shoutbox />} />
+            </header>
+            </div>
+
+            <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<Root/>}>
+        <Route path="questions" element={<Questions/>}/>
+        <Route path="tags" element={<Tags/>}/>
+        <Route path="shoutbox" element={<Shoutbox/>}/>
         </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+</Routes>
+</BrowserRouter>
+        </>
+    );
+
 }
