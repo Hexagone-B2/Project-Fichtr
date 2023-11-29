9b603e89534af72acbbc27500ec81537fd748885
Cyprien023 <cyprienluzuy@gmail.com>
2023-11-29 14:51:26 +0100

    mise à jour toolbar

--- Front/fichtr/src/Toolbar.jsx
+++ Front/fichtr/src/Toolbar.jsx
@@ -35,22 +35,17 @@ function Toolbar(props) {
                         type="search"
                         id="default-search"
                         className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Search Mockups, Logos..."
+                        placeholder="Faire une recherche"
                         required=""
                     />
-                    <button
-                        type="submit"
-                        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                        Search
-                    </button>
+                    
                 </div>
             </form>
 
-            <button className="btn-connecter">
-                <img src="./img/user-regular.svg" alt="user-icon" />
-                Connexion
-            </button>
+
+
+            <button type="button" className="btn-connecter focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+                Se connecter</button>
 
 
 
