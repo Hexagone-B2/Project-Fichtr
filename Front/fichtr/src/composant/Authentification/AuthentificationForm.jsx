b32c9093f2c71570bfdb0e7314205a42cd63d161
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2023-11-30 10:10:32 +0100

    bouton test cookie

--- Front/fichtr/src/composant/Authentification/AuthentificationForm.jsx
+++ Front/fichtr/src/composant/Authentification/AuthentificationForm.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function AuthentificationForm(props) {
-  let [userName, setUserName] = useState("");
+  let [mail, setmail] = useState("");
   let [password, setPassword] = useState("");
+
+  function handleClick(event) {
+    axios.post("http://10.2.7.20:3000/api/echo");
+
+    event.preventDefault();
+  }
 
   function handleChange(event) {
     switch (event.target.name) {
-      case "userName":
-        setUserName(event.target.value);
+      case "mail":
+        setmail(event.target.value);
         break;
       case "password":
         setPassword(event.target.value);
@@ -19,13 +25,13 @@ export default function AuthentificationForm(props) {
   }
 
   function handleSubmit(event) {
-    const userName = event.target[0].value;
+    const mail = event.target[0].value;
     const password = event.target[1].value;
     const headers = {
       "Content-Type": "application/json",
     };
     var to_send = {
-      username: userName,
+      mail: mail,
       password: password,
     };
     axios
@@ -40,51 +46,55 @@ export default function AuthentificationForm(props) {
   }
 
   return (
-    <form
-      method="post"
-      onSubmit={handleSubmit}
-      className="inscription-form w-2/5 m-24"
-    >
-      <div className="mb-4">
-        <label
-          htmlFor="userName"
-          className="block text-gray-700 text-sm font-bold mb-2"
-        >
-          Nom d'utilisateur
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          name="userName"
-          id="userName"
-          value={userName}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="password"
-          className="block text-gray-700 text-sm font-bold mb-2"
-        >
-          Mot de passe
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="password"
-          name="password"
-          id="password"
-          value={password}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="flex items-center justify-between">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
-        >
-          Envoyer
-        </button>
-      </div>
-    </form>
+    <>
+      <form
+        method="post"
+        onSubmit={handleSubmit}
+        className="inscription-form w-2/5 m-24"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="mail"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Nom d'utilisateur
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="mail"
+            id="mail"
+            value={mail}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Mot de passe
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Envoyer
+          </button>
+        </div>
+      </form>
+
+      <button onClick={handleClick}>butno</button>
+    </>
   );
 }
