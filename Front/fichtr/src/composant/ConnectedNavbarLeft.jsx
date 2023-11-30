ca2668cc092045f3ae66e45fb18f1ae840434417
Mathias Kowalski <slender.kowalski@gmail.com>
2023-11-30 14:09:05 +0100

    style update

--- Front/fichtr/src/composant/ConnectedNavbarLeft.jsx
+++ Front/fichtr/src/composant/ConnectedNavbarLeft.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, NavLink } from "react-router-dom";
 import Questions from "../pages/questions";
 import Tags from "../pages/tags";
 import Shoutbox from "../pages/shoutbox";
@@ -6,8 +6,6 @@ import MyQuestions from "../pages/my-questions";
 import MyResponses from "../pages/my-responses";
 import MyLikes from "../pages/my-likes";
 import MyAccount from "../pages/my-account";
-
-
 
 
 const Root = () => {
@@ -18,57 +16,37 @@ const Root = () => {
           <a class="flex items-center text-xs whitespace-nowrap px-11 text-gray-500 rounded-lg group">
             MENU
           </a>
-          <ul class="space-y-3 font-medium text-red-500">
+          <ul class="space-y-3 font-medium">
             <li>
-              <a class="flex items-center p-2 text-gray-900 hover:bg-orange-100 group">
-                <Link to="/questions" class="flex-1 ms-3 whitespace-nowrap">Questions</Link>
-              </a>
+              <Link to="/questions" class="flex p-2 whitespace-nowrap focus:bg-gray-300">Questions</Link>
             </li>
             <li>
-              <a class="flex items-center p-2 text-gray-900 hover:bg-orange-100 group">
-                <Link to="/tags" class="flex-1 ms-3 whitespace-nowrap">Tags</Link>
-              </a>
+              <Link to="/tags" class="flex p-2 whitespace-nowrap focus:bg-gray-300">Tags</Link>
             </li>
             <li>
-              <a class="flex items-center p-2 text-gray-900 hover:bg-orange-100 group">
-                <Link to="/shoutbox" class="flex-1 ms-3 whitespace-nowrap">Shoutbox</Link>
-              </a>
+              <Link to="/shoutbox" class="flex p-2 whitespace-nowrap focus:bg-gray-300">Shoutbox</Link>
             </li>
           </ul>
 
           <a class="flex items-center text-xs whitespace-nowrap px-11 text-gray-500 rounded-lg group">
             NAVIGATION PERSONNELLE
           </a>
-          <ul>
+          <ul class="space-y-3 font-medium">
             <li>
-              <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                </svg>
-                <Link to="/mes-questions" class="flex-1 ms-3 whitespace-nowrap">Mes Questions</Link>
-              </a>
+              <Link to="/mes-questions" class="flex p-2 whitespace-nowrap focus:bg-gray-300">Mes Questions</Link>
             </li>
             <li>
-              <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                  <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
-                </svg>
-                <Link to="/mes-reponses" class="flex-1 ms-3 whitespace-nowrap">Mes Réponses</Link>
-              </a>
+              <Link to="/mes-reponses" class="flex p-2 whitespace-nowrap focus:bg-gray-300">Mes Réponses</Link>
             </li>
             <li>
-              <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
-                </svg>
-                <Link to="/mes-likes" class="flex-1 ms-3 whitespace-nowrap">Mes Likes</Link>
-              </a>
+              <Link to="/mes-likes" class="flex p-2 whitespace-nowrap focus:bg-gray-300">Mes Likes</Link>
             </li>
             <li>
-              <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                
-                <img src="./img/user-regular.svg"/>
-                <Link to="/mon-compte" class="flex-1 ms-3 whitespace-nowrap">Mon Compte</Link>
+              <a class="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" height="16" width="14" viewBox="0 0 448 512">
+                  <path fill="#00000" d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+                </svg>
+                <Link to="/mon-compte" class="flex p-2 whitespace-nowrap focus:bg-gray-300">Mon Compte</Link>
               </a>
             </li>
           </ul>
