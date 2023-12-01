3c180ae185f3415d67551e563cdbf3051b4633fc
Mathias Kowalski <slender.kowalski@gmail.com>
2023-12-01 09:31:19 +0100

    refactored Routes to have a single router

--- Front/fichtr/src/composant/AppRouter.jsx
+++ Front/fichtr/src/composant/AppRouter.jsx
@@ -1,10 +1,27 @@
-import { BrowserRouter } from "react-router-dom";
-import LeftNavbar from "./LeftNavbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Questions from "../pages/questions";
+import Tags from "../pages/tags";
+import Shoutbox from "../pages/shoutbox";
+import MyQuestions from "../pages/my-questions";
+import MyResponses from "../pages/my-answers";
+import MyLikes from "../pages/my-likes";
+import MyAccount from "../pages/my-account";
+import NavbarLeft from "./LeftNavbar";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <LeftNavbar/>
+      <Routes>
+            <Route path="/" element={<NavbarLeft />}>
+                <Route path="questions" element={<Questions />} />
+                <Route path="tags" element={<Tags />} />
+                <Route path="shoutbox" element={<Shoutbox />} />
+                <Route path="my-questions" element={<MyQuestions />} />
+                <Route path="my-answers" element={<MyResponses />} />
+                <Route path="my-likes" element={<MyLikes />} />
+                <Route path="my-account" element={<MyAccount />} />
+            </Route>
+        </Routes>
     </BrowserRouter>
   );
 }
