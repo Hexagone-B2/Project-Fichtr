25f0dcd225a2fc1547a5da35f47c2991bddd2c44
Enzo Salson <drawkend@gmail.com>
2023-12-05 12:01:09 +0100

    je sais pas comment expliquer ce que jai fais mais cest pas important

--- Back/src/routes.js
+++ Back/src/routes.js
@@ -2,66 +2,63 @@ const router = require('express').Router();
 const multer = require('multer')
 const storage = require('./func/multerStorage');
 
-// Importation des endpoints
-const {getProfilePic} = require('./endpoints/getProfilePic');
-const {getPosts} = require('./endpoints/getPosts');
-const {echo} = require('./endpoints/echo');
-const {hello} = require('./endpoints/hello');
-const {updateProfilePic} = require('./endpoints/updateProfilePic');
-const {register} = require('./endpoints/register');
-const {login} = require('./endpoints/login');
-const {updateUnameMailBio} = require('./endpoints/updateUnameMailBio');
-const {getProfile} = require('./endpoints/getProfile');
-const {getUnameMailBio} = require('./endpoints/getUnameMailBio');
-const {getPostInfo} = require('./endpoints/getPostInfo');
-const {modifyPassword} = require("./endpoints/modifyPassword");
-const {getMyInfo} = require("./endpoints/getMyInfo");
-const {like} = require("./endpoints/like")
-const {unlike} = require("./endpoints/unlike")
-const {createSubject} = require("./endpoints/createSubject");
-const {getSubjects} = require("./endpoints/getSubjects");
-const {deleteSubject} = require("./endpoints/deleteSubject");
-const {isAuthenticated} = require("./endpoints/isAuthenticated");
-
 const upload = multer({storage: storage});
 
+const {getProfilePic} = require('./endpoints/getProfilePic');
 router.get("/getProfilePic", getProfilePic);
 
+const {getPosts} = require('./endpoints/getPosts');
 router.get("/getPosts", getPosts);
 
-router.post('/echo', echo);
-router.get('/echo', echo);
+const {echo} = require('./endpoints/echo');
+router.post('/echo', echo); router.get('/echo', echo);
 
+const {hello} = require('./endpoints/hello');
 router.get('/', hello);
 
+const {updateProfilePic} = require('./endpoints/updateProfilePic');
 router.post('/updateProfilePic', upload.single('file'), updateProfilePic)
 
+const {register} = require('./endpoints/register');
 router.post('/register', register)
 
+const {login} = require('./endpoints/login');
 router.post('/login', login)
 
+const {updateUnameMailBio} = require('./endpoints/updateUnameMailBio');
 router.post('/updateUnameMailBio', updateUnameMailBio)
 
+const {getProfile} = require('./endpoints/getProfile');
 router.post('/getProfile', getProfile);
 
+const {getUnameMailBio} = require('./endpoints/getUnameMailBio');
 router.post('/getUnameMailBio', getUnameMailBio)
 
+const {getPostInfo} = require('./endpoints/getPostInfo');
 router.post('/getPostInfo',getPostInfo);
 
+const {modifyPassword} = require("./endpoints/modifyPassword");
 router.post("/modifyPassword", modifyPassword);
 
+const {getMyInfo} = require("./endpoints/getMyInfo");
 router.post("/getMyInfo", getMyInfo);
 
+const {like} = require("./endpoints/like")
 router.post("/like", like);
 
+const {unlike} = require("./endpoints/unlike")
 router.post('/unlike',unlike)
 
+const {createSubject} = require("./endpoints/createSubject");
 router.post('/createSubject', createSubject)
 
+const {getSubjects} = require("./endpoints/getSubjects");
 router.post('/getSubjects', getSubjects)
 
+const {deleteSubject} = require("./endpoints/deleteSubject");
 router.post('/deleteSubject', deleteSubject)
 
+const {isAuthenticated} = require("./endpoints/isAuthenticated");
 router.post('/isAuthenticated', isAuthenticated)
 
 
