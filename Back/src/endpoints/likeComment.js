0f9d066bb51a7b955104cc2f6b890637cccdbb57
Enzo Salson <drawkend@gmail.com>
2023-12-07 17:04:23 +0100

    Ajout et modification comment,getComments,getLikesCountComment,likeComment

---
+++ Back/src/endpoints/likeComment.js
@@ -0,0 +1,21 @@
+const {checkAuth} = require("../func/checkAuth");
+const {executeSQL} = require("../func/mysql");
+module.exports.likeComment = (req,res)=>{
+    if (req.body.comment_id && req.headers.authorization){
+        checkAuth(req.headers.authorization,(error,decoded)=>{
+            if (error){
+                res.status(403).send('INVALID_TOKEN_OR_EXPIRED');
+            }else{
+                executeSQL('INSERT INTO LikesComment (user_id,comment_id) VALUES (?,?);',[decoded.id,req.body.comment_id],(error)=>{
+                    if (error){
+                        res.status(500).send('INTERNAL_ERROR');
+                    }else{
+                        res.send('OK');
+                    }
+                })
+            }
+        })
+    }else{
+        res.status(403).send('NOT_ALL_DATA')
+    }
+}
