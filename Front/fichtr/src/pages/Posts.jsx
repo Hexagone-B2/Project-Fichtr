e031f9a67898b516914f98205b41b685920ff2d8
Cyprien023 <cyprienluzuy@gmail.com>
2023-12-07 11:54:37 +0100

    update posts.jsx

--- Front/fichtr/src/pages/Posts.jsx
+++ Front/fichtr/src/pages/Posts.jsx
@@ -1,32 +1,30 @@
-import RightNavBar from "../component/Bar/RightNavBar";
-import NavbarLeft from "../component/Bar/LeftNavbar";
-import Toolbar from "../component/Bar/Toolbar";
-import Post from "../component/Post/Post";
-import Commentaire from "../component/Post/Commentaire";
+import Post from "../composant/Post";
+import Commentaire from "../composant/Commentaire";
+import Layout from "../pages/Layout";
+import {useEffect} from "react";
+
 
 export default function Posts(props) {
-  return (
-    <>
-      <Toolbar />
-      <div className="flex flex-row w-screen justify-between">
-        <div className="top-[8rem] relative">
-          <NavbarLeft />
-        </div>
+    useEffect(() => {
+        axios.post("http://enzo-salson.fr:3001/api/getComments")
+    }, []);
 
-        <div className="z-0 top-[8rem] relative w-full overflow-hidden bg-[#fafafa]">
-          <div className="mx-auto max-w-2xl p-8">
-            <Post id={1} />
-            <div className="flex-col p-1">
-              <Commentaire />
-              <Commentaire />
-            </div>
-          </div>
-        </div>
+    return (
 
-        <div className="top-[8rem] relative bg-[#fafafa]">
-          <RightNavBar />
-        </div>
-      </div>
-    </>
-  );
+
+        <Layout>
+
+                    <div className="mx-auto max-w-2xl p-8">
+                        <Post id={1} />
+                        <div className="flex-col p-1">
+                            <Commentaire />
+                            <Commentaire />
+
+                        </div>
+                    </div>
+
+        </Layout>
+
+
+    );
 }
