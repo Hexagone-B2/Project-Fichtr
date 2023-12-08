e899a4ec0edc2b4cf08ca1c67b3871cdcc3d06e6
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2023-12-08 22:56:28 +0100

    add Tag component

---
+++ Front/fichtr/src/composant/Tag.jsx
@@ -0,0 +1,7 @@
+export default function Tag({ title }) {
+  return (
+    <div className="border px-2 py-1 mr-1 bg-gray-300 text-gray-600 rounded-md w-fit text-sm">
+      {title}
+    </div>
+  );
+}
