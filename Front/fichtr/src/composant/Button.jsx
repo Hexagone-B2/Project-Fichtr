3173ac55fe6d82a2b9705accf7eeb73ddf768801
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2023-12-09 10:40:53 +0100

    add Button component

---
+++ Front/fichtr/src/composant/Button.jsx
@@ -0,0 +1,28 @@
+export default function Button({ title, handleButton, theme, icon, type }) {
+  function selectTheme(theme) {
+    switch (theme) {
+      case "primary":
+        return "bg-[#310046] hover:bg-[#510273]";
+      case "warning":
+        return "bg-[#F9AD3B] hover:bg-[#FFC060]";
+      case "danger":
+        return "bg-[#910000] hover:bg-[#BD0101]";
+      case "success":
+        return "bg-[#009143] hover:bg-[#00BA56]";
+      default:
+        return "bg-[#310046] hover:bg-[#510273]";
+    }
+  }
+  return (
+    <button
+      type={type ? type : ""}
+      className={`text-white flex justify-center rounded items-center w-[8rem] h-[2.5rem] gap-2  ${selectTheme(
+        theme
+      )}`}
+      onClick={handleButton}
+    >
+      {icon ? icon : null}
+      {title}
+    </button>
+  );
+}
