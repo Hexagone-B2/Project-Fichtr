6b2f28adbe272555f3c2d1a6d51355634b4f0820
Enzo Salson <github@enzo-salson.fr>
2023-12-10 22:24:01 +0100

    Supression de la config du websocket et importation du websocket depuis un autre fichier. FUCK CORS.

--- Back/src/index.js
+++ Back/src/index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.FIICHTR_PORT || 3000;
 const server = require('http').createServer(app);
-const socketio = require('socket.io')
-const io = socketio(server, {secure: true});
+require('./socket/websocket')(server);
+
 
 //DATA PARSER
 const bodyParser = require('body-parser');
@@ -15,32 +15,26 @@ const routes = require('./routes.js');
 //PRODUCTION
 const cors = require('cors');
 const helmet = require('helmet')
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: 'POST,GET'
+}));
 app.disable('x-powered-by')
 app.use(helmet({
     contentSecurityPolicy: false,
 }))
-
 
 // MIDDLEWARES
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json());
 app.use('/api',routes);
 
-io.on('connection', function (socket) {
-    console.log('A user connected');
-    socket.on('message', (data) => {
-        console.log(data);
-        io.emit('message', 'message bien reçu');
-    })
-});
-
+// Routes non api
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 
-//WEBSOCKET
 server.listen(port, () => {
     console.log('[*] Serveur WEB en écoute sur le port ' + port)
 })
