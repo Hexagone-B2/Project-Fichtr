f5de72dee2b72f2e30a2aa73ab29a36e65230bfa
Mathias Kowalski <slender.kowalski@gmail.com>
2023-12-14 16:17:04 +0100

    made usernames bigger and bold

--- Front/fichtr/src/composant/PrivateMessages.jsx
+++ Front/fichtr/src/composant/PrivateMessages.jsx
@@ -13,7 +13,7 @@ function ConversationList(){
                         <img crossOrigin="anonymous" class="w-16 h-16 rounded-xl" src={"https://dev.enzo-salson.fr/api/getProfilePic?id=" + conversation.pfp} alt={conversation.username} />
                         <div style={{ flexDirection: 'column'}} class="flex items-left space-x-2 rtl:space-x-reverse">
                             <div style={{ flexDirection: 'row'}} class="px-2.5">
-                                <span class="text-sm font-semibold text-gray-900">{conversation.username}</span>
+                                <span class="text-xl font-bold text-gray-900">{conversation.username}</span>
                                 <span class="text-sm px-2.5 font-normal text-gray-500">11:46</span>
                             </div>
                             <p class="text-sm font-normal py-2.5 text-gray-900">{conversation.content}</p>
