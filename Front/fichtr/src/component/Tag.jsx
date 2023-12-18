5522173756f686b364c145aa5eea272c77f3a29c
enzosln <github@enzo-salson.fr>
2023-12-19 00:13:41 +0100

    Résolution de bugs, ajout des tendances, des statistiques

--- Front/fichtr/src/component/Tag.jsx
+++ Front/fichtr/src/component/Tag.jsx
@@ -1,6 +1,6 @@
 export default function Tag({ title }) {
     return (
-        <kbd className="px-2 py-1.5 m-2 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg">
+        <kbd className="text-center px-2 py-1.5 m-2 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg">
             {title}
         </kbd>
     );
