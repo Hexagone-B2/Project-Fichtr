eaaf53f71172de4b10688cd75536ec5d15f1ceb6
enzosln <github@enzo-salson.fr>
2023-12-20 02:27:40 +0100

    Sécurité : mise en secret du mot de passe de la base de donnée

--- Back/src/func/mysql.js
+++ Back/src/func/mysql.js
@@ -1,11 +1,12 @@
 module.exports.executeSQL = (query, params, callback) => {
     const mysql = require('mysql');
+    require('dotenv').config();
     const connection = mysql.createConnection({
-        host: '51.159.9.88',
-        user: 'dbAdminFichtr',
-        password: '{>f34gN@#YU1x2dTpYCf',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
         database: 'fichtr',
-        port: 27085
+        port: process.env.DB_PORT
     });
     connection.connect((err) => {
         if (err) {
