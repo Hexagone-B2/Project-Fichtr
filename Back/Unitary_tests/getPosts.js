0f86c11f69cbb2ec26abcde74157f8d4a9be1c00
enzosln <github@enzo-salson.fr>
2023-12-22 11:07:42 +0100

    améliorations

--- Back/Unitary_tests/getPosts.js
+++ Back/Unitary_tests/getPosts.js
@@ -14,21 +14,7 @@ describe('API Tests', () => {
                 expect(err).to.be.null;
                 expect(res).to.have.status(200);
                 expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
-                expect(res.body).to.have.property('tab');
-
-                done();
-            });
-    });
-    it('https://dev.enzo-salson.fr/api/getPosts doit retourner un json de posts en requête post', (done) => {
-        chai
-            .request('https://dev.enzo-salson.fr')
-            .post('/api/getPosts')  // Utilisez .post() au lieu de .get() pour une requête POST
-            .send({nb: 0})
-            .end((err, res) => {
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
-                expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
-                expect(res.body).to.have.property('tab');
+                expect(res.body).to.be.an('array');
 
                 done();
             });
